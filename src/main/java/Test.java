c5c110d402d5686368cf7921119209ea312854e3
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-05-11 12:50:18 +0100

    Fir

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,16 @@
+import com.sun.source.doctree.SeeTree;
+
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         String s = "bla bla <alex@stackhowto.com> && mail:emily@gmail.com";
         String st = "alex@stackhowto.com";
         Matcher m = Pattern.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+").matcher(st);
@@ -10,5 +18,19 @@ public class Test {
         while (m.find()){
             System.out.println(m.group());
         }
+        String testing  = "War ended in 6724-10-1945, UN was established on 15-08-194789";
+        System.out.println(distinct(testing));
+
+    }
+
+    static int distinct(String statement) throws ParseException {
+        Matcher m = Pattern.compile("([ ]\\d{1,2}-\\d{1,2}-[0-9]{1,4})").matcher(statement);
+        Set<String> ans = new HashSet<>();
+        while (m.find()){
+            String[] val = m.group().split("-");
+            System.out.println(m.group());
+            ans.add(val[2]);
+        }
+        return ans.size();
     }
 }
